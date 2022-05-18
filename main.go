97a857334ed05dbdc3b2a3f7f05f4b4151208f40
Sergiu78 <turutaserghei@mail.ru>
2022-05-18 18:45:52 +0300

    select all from db

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 
@@ -16,7 +23,31 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM `articles`")
+	if err != nil {
+		panic(err)
+	}
+
+	posts = []Article{}
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+
+		// fmt.Println(fmt.Sprintf("Post: %s and ID: %d", post.Title, post.Id))
+		posts = append(posts, post)
+	}
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 func create(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
@@ -27,26 +58,31 @@ func create(w http.ResponseWriter, r *http.Request) {
 
 	t.ExecuteTemplate(w, "create", nil)
 }
+
 func save_article(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	anons := r.FormValue("anons")
 	full_text := r.FormValue("full_text")
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Fill in all fields of the form")
+	} else {
+		db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/golang")
+		if err != nil {
+			panic(err)
+		}
 
-	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/golang")
-	if err != nil {
-		panic(err)
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, full_text))
+		if err != nil {
+			panic(err)
+		}
+
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 
-	defer db.Close()
-
-	insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, full_text))
-	if err != nil {
-		panic(err)
-	}
-
-	defer insert.Close()
-
-	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func handleFunc() {
